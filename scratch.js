4abd39e745b73ca67865f99e3ea52b17e1568962
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2022-08-28 20:58:59 -0600

    Add 2.3 homework assignment

--- scratch.js
+++ scratch.js
@@ -1,5 +1,9 @@
 /* const data = '040011100012011010111100100100111300020300000'.split('') */
 const data = '0000000001123334555555555555555668889'.split('')
-data.forEach(c => {
+
+const pulseRates = '80 94 58 66 56 82 78 86 88 56 36 66 84 76 78 64 66 78 60 64'
+  .split(' ')
+  .sort()
+pulseRates.forEach(c => {
   console.warn(c)
 })
