cc72feebe509f93210e924338cc8a4ef75b1bd20
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2022-09-03 14:40:33 -0600

    Add Homework 3.1

--- scratch.js
+++ scratch.js
@@ -5,5 +5,12 @@ const pulseRates = '80 94 58 66 56 82 78 86 88 56 36 66 84 76 78 64 66 78 60 64'
   .split(' ')
   .sort()
 pulseRates.forEach(c => {
+  /* console.warn(c) */
+})
+
+const phenotypes = '2 1 1 1 1 1 1 4 1 2 2 1 2 3 3 2 3 1 3 1 3 1 3 2 2'
+  .split(' ')
+  .sort()
+phenotypes.forEach(c => {
   console.warn(c)
 })
